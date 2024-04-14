4eb3c59dd1fc0fae0b3391a839e93f305f931da5
Koval <hazardsales@gmail.com>
2024-04-14 22:50:27 +0200

    appconfig refactor

--- src/main/java/com/example/awslambda/configuration/AppConfig.java
+++ src/main/java/com/example/awslambda/configuration/AppConfig.java
@@ -16,15 +16,13 @@ public class AppConfig {
     private String accessKey;
     @Value("${aws.s3.secret-key}")
     private String secretKey;
-
     @Value("${aws.s3.endpoint}")
     private String s3endpoint;
-
     @Value("${aws.s3.region}")
     private String s3region;
 
     @Bean
-    public DynamoDbClient getDynamoDbClient() {
+    public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
             .region(Region.EU_CENTRAL_1)
             .credentialsProvider(StaticCredentialsProvider.create(
@@ -33,7 +31,7 @@ public class AppConfig {
     }
 
     @Bean
-    public S3Client createS3Client() {
+    public S3Client S3Client() {
         return S3Client.builder()
             .region(Region.EU_CENTRAL_1)
             .credentialsProvider(StaticCredentialsProvider.create(
